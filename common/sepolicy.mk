d078ee10ba040aab6e8c4bda55d23b6652d41c07
GIoKas93 <giorgio_castelluccilibero.it>
2021-03-04 07:26:03 +0000

    adapt to nitrogen

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from dot
+# inherit from nitrogen
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -16,20 +16,20 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/dot/sepolicy/common/public
+    device/nitrogen/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/dot/sepolicy/common/private
+    device/nitrogen/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/dot/sepolicy/common/dynamic \
-    device/dot/sepolicy/common/system
+    device/nitrogen/sepolicy/common/dynamic \
+    device/nitrogen/sepolicy/common/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/dot/sepolicy/common/dynamic \
-    device/dot/sepolicy/common/vendor
+    device/nitrogen/sepolicy/common/dynamic \
+    device/nitrogen/sepolicy/common/vendor
 endif
 
 # Selectively include legacy rules defined by the products
--include device/dot/sepolicy/legacy-common/sepolicy.mk
+-include device/nitrogen/sepolicy/legacy-common/sepolicy.mk
--- legacy-common/sepolicy.mk
+++ legacy-common/sepolicy.mk
@@ -5,5 +5,5 @@
 
 ifeq ($(TARGET_HAS_LEGACY_CAMERA_HAL1), true)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/dot/sepolicy/legacy-common/public/legacy-camera-hal1
+    device/nitrogen/sepolicy/legacy-common/public/legacy-camera-hal1
 endif
